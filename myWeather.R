d6dbfdbc37a385dac979a82c70f8ab0c1377c4db
bcoffe <brent.coffey@nih.gov>
2014-01-05 07:20:57 -0500

    working on the image.

--- myWeather.R
+++ myWeather.R
@@ -5,23 +5,36 @@ kelvinToFarenheit <- function (kelvinTemp) {
   (kelvinTemp - 273.15)*1.8 + 32;
 }
 
+getWeatherImage <- function (imageName) {
+  f <- CFILE(paste("img/", key, ".png", sep=''), mode="wb");
+  curlPerform( url = 
+               paste("http://openweathermap.org/img/w/", imageName, ".png", sep=''), 
+               writedata = f@ref);
+  close(f);
+}
+
 args <- commandArgs(trailingOnly = TRUE)
 latitude <- args[1];
 longitude <- args[2];
+key <- args[3];
 
-currentWeather <- getURL(paste("http://api.openweathermap.org/data/2.5/weather?lat=",latitude, "&lon=", longitude, "&APPID=10c94e0253609fd9f78e207f831b2b09", sep=''));
+currentWeather <- getURL(paste("http://api.openweathermap.org/data/2.5/weather?lat=",
+                               latitude, 
+                               "&lon=", longitude, 
+                               "&APPID=10c94e0253609fd9f78e207f831b2b09", sep=''));
 weatherData <- fromJSON(currentWeather);
 
 farenheitCurrentTemp <- kelvinToFarenheit(weatherData$main["temp"]);
 farenheitHighTemp <- kelvinToFarenheit(weatherData$main["temp_max"]);
 farenheitLowTemp <- kelvinToFarenheit(weatherData$main["temp_min"]);
+currentCondition <- weatherData$weather[[1]]$description;
 
-cat( "Conditions for weatherData$name\n",
-    "Current Temp: ", farenheitCurrentTemp, "° F\n",
-    "High Temp: ", farenheitHighTemp, "° F\n",
-    "Low Temp: ", farenheitLowTemp, "° F.", sep='');
-print(weatherData);
+# Writes out the weather. Modify this to write out the weather in whatever format you prefer.
+cat( weatherData$name,"\n",
+     "Current Condition: ", currentCondition, "\n",
+     "Current Temp: ", farenheitCurrentTemp, "° F\n",
+     "High Temp: ", farenheitHighTemp, "° F\n",
+     "Low Temp: ", farenheitLowTemp, "° F.", sep='');
 
-f = CFILE("img/CurrentWeather.png", mode="wb");
-curlPerform(url = paste("http://openweathermap.org/img/w/", weatherData$weather[[1]]$icon, ".png", sep=''), writedata = f@ref);
-close(f);
+# Retrieves the image based on the weather returned in getURL
+getWeatherImage( weatherData$weather[[1]]$icon);
