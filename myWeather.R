b3f63a911120f95b69e9a655bd5391fcbf4d8ad8
bcoffe <brent.coffey@nih.gov>
2014-01-05 15:13:25 -0500

    adding hourly forecast code

--- myWeather.R
+++ myWeather.R
@@ -1,16 +1,11 @@
 suppressMessages(library('RJSONIO'));
 suppressMessages(library('RCurl'));
 
-kelvinToFarenheit <- function (kelvinTemp) {
-  (kelvinTemp - 273.15)*1.8 + 32;
-}
+source('~/Development/rprogramming/CommandLineWeatherR/getWeatherImage.R');
+source('~/Development/rprogramming/CommandLineWeatherR/kelvinToFarenheit.R');
 
-getWeatherImage <- function (imageName, imagePath) {
-  imageLocation <- paste("http://openweathermap.org/img/w/", imageName, ".png", sep='');
-  f <- CFILE(paste(imagePath, ".png", sep=''), mode="wb");
-  curlPerform(url = imageLocation, writedata = f@ref);
-  msg.trap <- capture.output(suppressMessages(close(f)));
-}
+# Change to your id. Getting an ID from OpenWeather.org is Free
+appId <- "10c94e0253609fd9f78e207f831b2b09";
 
 args <- commandArgs(trailingOnly = TRUE);
 latitude <- args[1];
@@ -20,7 +15,7 @@ imagePath <- args[3];
 currentWeather <- getURL(paste("http://api.openweathermap.org/data/2.5/weather?lat=",
                                latitude, 
                                "&lon=", longitude, 
-                               "&APPID=10c94e0253609fd9f78e207f831b2b09", sep=''));
+                               "&APPID=", appId, sep=''));
 weatherData <- fromJSON(currentWeather);
 
 farenheitCurrentTemp <- kelvinToFarenheit(weatherData$main["temp"]);
@@ -29,7 +24,7 @@ farenheitLowTemp <- kelvinToFarenheit(weatherData$main["temp_min"]);
 currentCondition <- weatherData$weather[[1]]$description;
 
 # Writes out the weather. Modify this to write out the weather in whatever format you prefer.
-cat( "Latest Weather Station:", weatherData$name,"\n",
+cat( "Latest Weather Station: ", weatherData$name,"\n",
      "Current Condition: ", currentCondition, "\n",
      "Current Temp: ", farenheitCurrentTemp, "° F\n",
      "High Temp: ", farenheitHighTemp, "° F\n",
